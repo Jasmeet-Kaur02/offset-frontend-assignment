be4099576b90f3e34a0bea30f5d8f9d5738e693b
JasmeetKaurx <jasskaur.dha@gmail.com>
2025-09-26 19:24:17 +0530

    basic build fixes

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      protocolImports: true,
-    }),
-  ],
-  optimizeDeps: {
-    include: ["pako"],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
+  plugins: [react()],
   server: {
     port: 3000,
   },
